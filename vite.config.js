ba4e2b086b6bcfa15892c9a0e73a3f776eb96cdb
jayrenzgit <jayrenzkolven@gmail.com>
2024-11-24 16:51:46 +0800

    sss

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  //base: '/JourneyMakers/', // Use your repository name here
+  base: '/JourneyMakers/', // Use your repository name here
 });
